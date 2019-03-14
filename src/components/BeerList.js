86c3104a42013266c83317d6547972dabb882563
rsmelo <rodrigosm87@gmail.com>
2019-03-14 02:15:28 -0300

    beerList: add component

---
+++ src/components/BeerList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const BeerList = ({ data, loading}) => (
+  <div>
+    {loading && (
+      <p>Please wait...</p>
+    )}
+    {!loading &&  (
+      <div>
+        <p>Got {data.length} beer(s)</p>
+      </div>
+    )}
+  </div>
+)
+
+
+export default connect(state => state.beers)(BeerList)
